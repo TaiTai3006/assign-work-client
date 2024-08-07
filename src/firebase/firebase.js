2a2a56c04c2d53f254e874cb5a20a7ac39dcce3d
TaiTai3006 <101327980+TaiTai3006@users.noreply.github.com>
2024-08-07 22:22:18 +0700

    signInWith gg

---
+++ src/firebase/firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB5LY4eI-mHa4DlRUg9wwMwYsth1jd6LhU",
+  authDomain: "assign-work-59dd4.firebaseapp.com",
+  projectId: "assign-work-59dd4",
+  storageBucket: "assign-work-59dd4.appspot.com",
+  messagingSenderId: "1017639265146",
+  appId: "1:1017639265146:web:35bdf846dc7072ef99ecc3",
+  measurementId: "G-FCF7NHDZSH"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
+export { auth, provider };
--- src/screen/Login.screen.js
+++ src/screen/Login.screen.js
@@ -1,13 +1,25 @@
 import React from "react";
 import GoogleButton from "react-google-button";
+import { auth, provider } from "../firebase/firebase";
+import { signInWithPopup } from 'firebase/auth';
 
 const Login = () => {
+  const signin = () => {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const user = result.user;
+        console.log(result)
+        console.log('User info:', user);
+      })
+      .catch((error) => {
+        console.error('Error during sign-in:', error);
+      });
+}
+
   return (
     <div className="container vh-100 d-flex justify-content-center align-items-center">
     <GoogleButton
-      onClick={() => {
-        console.log("Google button clicked");
-      }}
+      onClick={signin}
     />
   </div>
   );
