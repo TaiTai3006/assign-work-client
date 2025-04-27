29179815f46d88dc0009599df04973abb74b72fb
TaiTai3006 <101327980+TaiTai3006@users.noreply.github.com>
2025-04-27 21:33:33 +0900

    test

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -16,6 +16,10 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+  prompt: "select_account"
+});
+
 const storage = getStorage(app);
 
 export { auth, provider, storage };
