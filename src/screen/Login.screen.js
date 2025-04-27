6eb585de4af77bdfd42a9961dd8c3ad90705a2d2
TaiTai3006 <101327980+TaiTai3006@users.noreply.github.com>
2025-04-27 20:56:59 +0900

    test

--- src/screen/Login.screen.js
+++ src/screen/Login.screen.js
@@ -2,32 +2,29 @@ import React from "react";
 import GoogleButton from "react-google-button";
 import { auth, provider } from "../firebase/firebase";
 import { signInWithPopup } from "firebase/auth";
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 import { useLogin } from "../redux/action/auth.action";
-
 
 const Login = () => {
   const author = useSelector((state) => state.auth.value);
   const { login } = useLogin();
 
-  const signin = () => {
-    signInWithPopup(auth, provider)
-      .then((result) => {
-        const user = result.user;
-        console.log(result);
-        console.log("User info:", user);
-        login(user.email, user.accessToken);
-      })
-      .catch((error) => {
-        console.error("Error during sign-in:", error);
-      });
+  const handleSignIn = async () => {
+    try {
+      const result = await signInWithPopup(auth, provider);
+      const { email, accessToken } = result.user;
+      console.log("User signed in:", result.user);
+      login(email, accessToken);
+    } catch (error) {
+      console.error("Sign-in error:", error);
+    }
   };
 
-  console.log(author);
+  console.log("Auth State:", author);
 
   return (
     <div className="container vh-100 d-flex justify-content-center align-items-center">
-      <GoogleButton onClick={signin} />
+      <GoogleButton onClick={handleSignIn} />
     </div>
   );
 };
